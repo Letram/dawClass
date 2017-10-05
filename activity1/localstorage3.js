c5a76098b2d494d3ddf69b0712d503a48d4af96c
Carlos Martel Lamas <carlos.martel105@alu.ulpgc.es>
2017-10-05 13:45:54 +0100

    Second DAW activity ver2

--- activity1/localstorage3.js
+++ activity1/localstorage3.js
@@ -3,7 +3,9 @@ function sendNote() {
     if(localStorage.getItem("index") === null)localStorage.setItem("index", "0");
     index = parseInt(localStorage.getItem("index"));
     localStorage.setItem(index, nota.value);
+    showNote(index);
     localStorage.setItem("index", ++index);
+    /*
     li = document.createElement("li");
     li.innerHTML=nota.value;
     li.setAttribute("id", index);
@@ -12,22 +14,26 @@ function sendNote() {
         li.parentNode.removeChild(li);
     });
     document.getElementById("notas").appendChild(li);
+    */
 }
 
 function showNotes(){
+    document.getElementById("notas").innerHTML='';
     for (var index in localStorage){
-        if(index.value == "index")continue;
+        if(index.value === "index")continue;
+        showNote(index);
+    }
+}
+function showNote(index){
         li = document.createElement("li");
         li.innerHTML=localStorage.getItem(index);
         li.setAttribute("id", index);
         document.getElementById("notas").appendChild(li);
         li.addEventListener("click", function(){
             localStorage.removeItem(li.getAttribute("id"));
-            li.parentNode.removeChild(li);
+            showNotes();
         });
-    }
 }
-
 function removeAllNotes(){
     ul = document.getElementById("notas");
     while (ul.firstChild) {
