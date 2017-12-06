8a4b667996a817462e6922458ce19eb11248fc32
Carlos Martel Lamas <carlos.martel105@alu.ulpgc.es>
2017-12-06 22:58:48 +0000

    Some more JS functions added
    "more" button now has a JS function to show a new div with more "friends"

--- DAWProject2/Profile/assets/js/scripts.js
+++ DAWProject2/Profile/assets/js/scripts.js
@@ -2,13 +2,13 @@ $(document).ready(function() {
 
 	tripList = $('#trip-list li');
 	degrees = [];
-	display = []
+	display = [];
+	buttonState = 1;
 	$('#trip-list li').each(function (index) {
 		degrees.push(45);
 		display.push(0);
 		icon = $(this).children('i');
 		icon.click(function () {
-			console.log(display);
 			//icon rotates 45deg
 			$(this).css({'transform' : 'rotate('+ degrees[index] +'deg)'});
 			$(this).css({'transition' : 'all 0.25s ease-in-out'});
@@ -25,3 +25,22 @@ $(document).ready(function() {
         });
 	});
 });
+	function showMoreFriends() {
+        moreFriends = $('#moreFriends');
+        button = $('#moreFriendsButton');
+
+        //More = 1 , Less = 0
+		if(buttonState === 1){
+			button.html("Less");
+			buttonState = 0;
+		}else{
+			button.html("More");
+			buttonState = 1;
+		}
+		//we show a div with more friends
+        if(moreFriends.css('display') === 'none')
+            moreFriends.show(250);
+        else
+            moreFriends.hide(250);
+
+    }
